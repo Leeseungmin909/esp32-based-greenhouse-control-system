4506e66a4e5c8353a361f66f1dfe276570008d17
leeseungmin909 <cldkdi1@naver.com>
2026-05-29 21:32:55 +0900

    feat: systemd로 falsk를 bg 켜두기 위한 reloader 프로세스 추가

--- app.py
+++ app.py
@@ -342,4 +342,4 @@ def get_device_log_data():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
